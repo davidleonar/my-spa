9f369f8b359949a3dee0fb2b832fdf1ea6906a82
David Paniagua <davidleonar@gmail.com>
2025-02-24 16:57:26 -0500

    meta tags para despripcion inicio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -13,8 +14,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "BTC Services",
-  description: "Servicios BTC de confianza",
+  title: "DPS Direct Provider Services",
+  description: "Asegura tus cripto activos con David",
 };
 
 export default function RootLayout({
@@ -24,6 +25,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/logo.png" />
+        <meta property="og:title" content="DPS Direct Provider Services" />
+        <meta property="og:description" content="Asegura tus cripto activos con David" />
+        <meta property="og:image" content="/logo.png" />
+        <meta property="og:url" content="https://rendimientos-5dbb9.web.app/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="DPS Direct Provider Services" />
+        <meta name="twitter:description" content="Asegura tus cripto activos con David" />
+        <meta name="twitter:image" content="/logo.png" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
