2b132473c676a79d5d6b87eba5f17c16a7a4b363
David <davidleonar@gmail.com>
2025-11-12 12:35:59 -0500

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,11 +26,19 @@ export default function RootLayout({
   return (
     <html>
       <Head>
-        <link rel="icon" href="/logo.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/AppIcon.appiconset/180.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/AppIcon.appiconset/152.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/AppIcon.appiconset/120.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/AppIcon.appiconset/57.png" />
+        <link rel="icon" type="image/png" sizes="1024x1024" href="/AppIcon.appiconset/1024.png" />
+        <link rel="icon" type="image/png" sizes="180x180" href="/AppIcon.appiconset/180.png" />
+        <link rel="icon" type="image/png" sizes="152x152" href="/AppIcon.appiconset/152.png" />
+        <link rel="icon" type="image/png" sizes="120x120" href="/AppIcon.appiconset/120.png" />
+        <link rel="icon" type="image/png" sizes="57x57" href="/AppIcon.appiconset/57.png" />
         <meta property="og:title" content="Rendimientos App" />
         <meta property="og:description" content="Ahorra BTC de manera soberana" />
-        <meta property="og:image" content="/logo.png" />
-        <meta property="og:url" content="https://rendimientos-5dbb9.web.app/" />
+        <meta property="og:image" content="/AppIcon.appiconset/1024.png" />
+        <meta property="og:url" content="https://rendimientos.net/" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Rendimientos App" />
